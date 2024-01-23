2213c3ef7e8e2c98aedfe9cd0be7ba0b2dd2a58f
LuisGustavoDev <luisgustavodev@hotmail.com>
2024-01-23 05:09:39 -0300

    adcionando variáveis e realização dos calculos

--- desafio36/desafio36.py
+++ desafio36/desafio36.py
@@ -1,5 +1,10 @@
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 #Calcule o valor da prestação mensal sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado
 
-valor_casa = float(input('Digite o valor da casa:'))
-salário = float(input('Digite o seu salário: '))
+valor_casa = float(input('Digite o valor da casa. R$'))
+salário = float(input('Digite o seu salário R$'))
+anos = int(input('Digite em quantos anos irá pagar: '))
+
+meses = anos * 12
+prestacoes = valor_casa // meses
+print(prestacoes) 

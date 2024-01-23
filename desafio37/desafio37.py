6b0749adb9e07e9d30c1f6f071675f414c4bad40
LuisGustavoDev <luisgustavodev@hotmail.com>
2024-01-23 05:35:39 -0300

    Criando uma interface para o usuário e inserindo input

---
+++ desafio37/desafio37.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
+# 1- para bara binário 2- para octal 3- para hexadecimal
+
+print("""Conversor numérico:
+1- Binário
+2- Octal
+3- Hexadecimal""")
+
+escolha = input('Escolha um dos números: ')
